00be9526d3f5b725b3b74416ceb198e5aabdfa01
devkoepeli <devrim_kopeli@hotmail.com>
2023-12-31 16:37:19 +0100

    first three sections added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent,
+    HowToOrderComponent,
+    OurMenuComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
